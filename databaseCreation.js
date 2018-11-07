ac11596a771f3edfa7e2a5b7eff6dafa73f49166
Rafael Echeverria <rafael.echeverria@controlrisks.com>
2018-11-07 14:39:08 -0500

    - Finish Database scripts - add handler for creating Work Orders and Vehicles - add function to serverless yaml for vehicles, owners and work - add package.json file

--- databaseCreation.js
+++ databaseCreation.js
@@ -1,46 +1,70 @@
 // CREATE TABLE WORK ORDERS
 module.exports.CREATE_TABLE_WORKORDERS= () => {
-return `create table if not exists work_orders(
-    id int primary key auto_increment,
-    date datetime not null,
-    type_id int not null,
-    status_id int not null,
-    mechanic_id int not null,
-    vehicle_id int not null,
-    symptoms varchar(255) not null
-)`};
+    return `create table if not exists work_orders(
+        id int primary key auto_increment,
+        date datetime not null,
+        type_id int not null,
+        status_id int not null,
+        mechanic_id int not null,
+        vehicle_id int not null,
+        symptoms varchar(255) not null
+    )`
+};
 
 // CREATE TABLE VEHICLES
 module.exports.CREATE_TABLE_VEHICLES = () => {
-return `create table if not exists vehicles(
-    id int primary key auto_increment,
-    plate varchar(8) not null,
-    maker varchar(48) not null,
-    model varchar(48) not null,
-    color varchar(48) not null,
-    vin varchar(255) not null,
-    owner_id int not null
-)`};
+    return `create table if not exists vehicles(
+        id int primary key auto_increment,
+        plate varchar(8) not null,
+        maker varchar(48) not null,
+        model varchar(48) not null,
+        color varchar(48) not null,
+        vin varchar(255) not null,
+        owner_id int not null
+    )`
+};
 
 // CREATE TABLE OWNERS
 module.exports.CREATE_TABLE_OWNERS = () => {
-return `create table if not exists owners(
-    id int primary key auto_increment,
-    name varchar(128) not null,
-    email varchar(128) not null,
-    phone varchar(16) not null
-)`};
+    return `create table if not exists owners(
+        id int primary key auto_increment,
+        name varchar(128) not null,
+        email varchar(128) not null,
+        phone varchar(16) not null
+    )`
+};
 
 // CREATE TABLE MECHANICS
-
+module.exports.CREATE_TABLE_MECHANICS = () => {
+    return `create table if not exists mechanics(
+        id int primary key auto_increment,
+        name varchar(128) not null        
+    )`
+};
 
 // CREATE TABLE TYPES
-
+module.exports.CREATE_TABLE_TYPES = () => {
+    return `create table if not exists types(
+        id int primary key auto_increment,
+        name varchar(64) not null
+    )`
+};
 
 // CREATE TABLE STATUS
-
+module.exports.CREATE_TABLE_STATUS = () => {
+    return `create table if not exists status(
+        id int primary key auto_increment,
+        name varchar(64) not null
+    )`
+};
 
 // CREATE TABLE WORKS
-// id int primary key auto_increment,
-// work_order_id int not null,
-// price decimal(5,2) not null
+module.exports.CREATE_TABLE_WORKS = () => {
+    return `create table if not exists works(
+        id int primary key auto_increment,
+        work_order_id int not null,
+        price decimal(5,2) not null,
+        date datetime not null,
+        comments varchar(255)
+    )`
+}
--- handler.js
+++ handler.js
@@ -3,7 +3,15 @@ const mysql = require('promise-mysql');
 const dbconfig = require('./dbconfig');
 const dbscripts = require('./databaseCreation');
 
-module.exports.createDatabase = async (event, context) => {
+const CreateResponse = (code, body) => {
+  var response = {
+    statusCode: code,
+    body: JSON.stringify(body)
+  }
+  return response;
+};
+
+module.exports.createDatabase = (event, context) => {
   var connection;
   mysql.createConnection(dbconfig)
     .then(conn => {
@@ -17,13 +25,30 @@ module.exports.createDatabase = async (event, context) => {
       connection.query(dbscripts.CREATE_TABLE_OWNERS());
       console.log('Table Owners created');
   }).then(()=>{
-      connection.query();
-      console.log('Table Mechanics created;');
+      connection.query(dbscripts.CREATE_TABLE_MECHANICS());
+      console.log('Table Mechanics created');
+  }).then(()=>{
+      connection.query(dbscripts.CREATE_TABLE_TYPES());
+      console.log('Table Types created');
+  }).then(()=>{
+      connection.query(dbscripts.CREATE_TABLE_STATUS());
+      console.log('Table Status created');
+  }).then(()=>{
+      connection.query(dbscripts.CREATE_TABLE_WORKS());
+      console.log('Table Works created');
   })
   .then(()=>{
     connection.end();
   });
-  return {
-    statusCode: 200   
-  };
+  context.done(null,{message: 'Success'});
 };
+
+module.exports.createWorkOrder = async (event, context, callback) => {  
+  const {type_id, status_id, mechanic_id, vehicle_id, symptoms} = JSON.parse(event.body);  
+  return CreateResponse(201,{id: 1});
+};
+
+module.exports.createVehicle = async (event, context) => {
+  const {plate, maker, model, color, vin, owner_id} = JSON.parse(event.body);
+  return CreateResponse(201, {id: 1});
+}
